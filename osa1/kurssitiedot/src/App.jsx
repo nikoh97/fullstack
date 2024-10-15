e6b48524e34a8899021958c0c8c9c12afdb1bf5a
Niko Petteri Hirvonen <niko.p.hirvonen@student.jyu.fi>
2024-10-15 21:21:56 +0300

    Tehty tehtävä 1.3

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -9,12 +9,12 @@ return(
 } 
 
 const Content = (props) => {
-
+console.log(props)
 return (
   <div>
-    <Part teksti={props.teksti1} harjoitus={props.harjoitus1}/>
-    <Part teksti={props.teksti2} harjoitus={props.harjoitus2}/>
-    <Part teksti={props.teksti3} harjoitus={props.harjoitus3}/>
+    <Part teksti={props.part1.name} harjoitus={props.part1.exercises}/>
+    <Part teksti={props.part2.name} harjoitus={props.part2.exercises}/>
+    <Part teksti={props.part3.name} harjoitus={props.part3.exercises}/>
   </div>
 )
 
@@ -40,20 +40,30 @@ return (
 
 const App = () => {
   
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development'
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   
   return (
     <div>
       <Header nimi='Half Stack application development'/>
       
-      <Content teksti1={part1} harjoitus1={exercises1} teksti2={part2} harjoitus2={exercises2}teksti3={part3} harjoitus3={exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
       
-      <Total yht= {exercises1 + exercises2 + exercises3}/>
+      <Total yht= {part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
