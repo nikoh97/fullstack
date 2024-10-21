d3d480965c6e09031ecb9e3d326180798255b4c2
Niko Petteri Hirvonen <niko.p.hirvonen@student.jyu.fi>
2024-10-21 20:24:15 +0300

    Tehty tehtävä 2.1

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,45 @@
+const Course = (props) => {
+  const otsikko = props.course.name;
+  return (
+    <div>
+      <Header nimi={otsikko} />
+      <Content courses={props.course.parts} />
+      <Total course={props.course} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.nimi}</h1>;
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      {props.courses.map((value) => {
+        return (
+          <Part part={value} key={value.id} />
+        );
+      })}
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  let sum = 0;
+  props.course.parts.forEach((value) => {
+    sum += value.exercises;
+  });
+  return <p>Number of exercises {sum} </p>;
+};
+
+export default Course;
