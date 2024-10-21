4b52b2dd642623138b5c9e0cb34a0a56c1e38041
Niko Petteri Hirvonen <niko.p.hirvonen@student.jyu.fi>
2024-10-21 23:20:57 +0300

    Tehty tehtävä 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+
+    const noteObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(noteObject));
+    setNewName("");
+  };
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((value, i) => {
+          return <p key={i}>{value.name}</p>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
