1e084c590119be290b5006ff60197ff16b8bc40d
Niko Petteri Hirvonen <niko.p.hirvonen@student.jyu.fi>
2024-10-24 21:02:21 +0300

    Tehty tehtävä 2.11

--- osa2/puhelinluettelo/src/main.jsx
+++ osa2/puhelinluettelo/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
