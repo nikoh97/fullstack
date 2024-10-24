1e9196364b3c7a24cb9fc4a167f22dd889f38f9b
Niko Petteri Hirvonen <niko.p.hirvonen@student.jyu.fi>
2024-10-24 19:28:46 +0300

    Tehty tehtävä 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,36 +1,77 @@
 import { useState } from "react";
 
+const Persons = (props) => {
+  return (
+    <div>
+      {props.lista.map((value, i) => {
+        if (value.name.toLowerCase().includes(props.filtteri.toLowerCase())) {
+          return (
+            <p key={i}>
+              {value.name} {value.number}
+            </p>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.submit}>
+      <div>
+        name: <input value={props.value} onChange={props.onChange} />
+      </div>
+      <div>
+        number: <input value={props.nvalue} onChange={props.nonChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Filter = (props) => {
+  return (
+    <form>
+      <div>
+        filter shown with:{" "}
+        <input value={props.value} onChange={props.onChange} />
+      </div>
+    </form>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
 
   const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("")
-  const [newFilter, setNewFilter] = useState("")
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     console.log("button clicked", event.target);
 
     if (persons.find((value) => value.name === newName) != undefined) {
-      window.alert( `${newName} on jo lisätty`);
+      window.alert(`${newName} on jo lisätty`);
       return;
     }
 
     const person = {
-      name: newName, number: newNumber,
+      name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(person));
     setNewName("");
     setNewNumber("");
   };
-
-  
-
 
   const handlePersonChange = (event) => {
     console.log(event.target.value);
@@ -38,43 +79,29 @@ const App = () => {
   };
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
 
-  const handleFilterChange = (event) =>{
-    console.log(event.target.value)
-    setNewFilter(event.target.value)
-  }
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          filter shown with: <input value={newFilter} onChange={handleFilterChange} />
-        </div>
-      </form>
+      <Filter value={newFilter} onChange={handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        value={newName}
+        nvalue={newNumber}
+        onChange={handlePersonChange}
+        nonChange={handleNumberChange}
+        submit={addPerson}
+      />
       <h2>Numbers</h2>
-      <div>
-        {persons.map((value, i) => {
-          if (value.name.toLowerCase().includes(newFilter.toLowerCase())) {
-          return <p key={i}>{value.name} {value.number}</p>;
-          }
-        })}
-      </div>
+      <Persons lista={persons} filtteri={newFilter} />
     </div>
   );
 };
